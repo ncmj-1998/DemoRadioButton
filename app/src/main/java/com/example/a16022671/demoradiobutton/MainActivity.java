251de1f1303962f5f7d5fef7a66342cb24c682a8
16022671 <16022671@myrp.edu.sg>
2018-04-17 16:45:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022671/demoradiobutton/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a16022671.demoradiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton)findViewById(selectedButtonId);
+
+                Toast.makeText(getBaseContext(),rb.getText(),Toast.LENGTH_LONG).show();
+                
+            }
+        });
+
+    }
+}
